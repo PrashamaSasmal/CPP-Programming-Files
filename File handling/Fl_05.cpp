533980f9a5b8d5a762605e0499d9651c794a99a1
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-21 15:13:04 +0530

    File position indicators(tellg,seekg)

---
+++ File handling/Fl_05.cpp
@@ -0,0 +1,25 @@
+//file position indicators for get operation(tellp,seekg)
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    ifstream file("abc.txt",ios::in);
+    //append mode is here for appened text to the file
+    if(!file.is_open())
+    {
+        cout<<"File creation failed.";
+    }
+    else
+    {  
+        cout<<file.tellg()<<endl;;      //tell us where form we gonna to read file
+        string line;
+        file.seekg(3);      //getting or reading operation started form 3rd position of string
+        getline(file,line); 
+        cout<<line<<endl;
+        file.close();  
+        
+    }
+    return 0;
+
+}    
