90cfb9c3fb3f1bf09401f57ce07988d92a5f61ce
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-21 14:27:31 +0530

    Append text to a file

---
+++ File handling/Fl_04.cpp
@@ -0,0 +1,23 @@
+//append text to a  file  
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    fstream file("abc.txt",ios::in|ios::out|ios::app);
+    //append mode is here for appened text to the file
+    if(!file.is_open())
+    {
+        cout<<"File creation failed.";
+    }
+    else
+    {  
+        
+        file<<"\nHello!!Here we use append mode to add text to the file.";
+        cout<<"Writing to the file is done successfully.";
+        file.close();  
+        
+    }
+    return 0;
+
+}
