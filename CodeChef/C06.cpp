d235f1f0f4d8ad862376dbfc88fc3120b90856d2
PrashamaSasmal <80055409+PrashamaSasmal@users.noreply.github.com>
2022-05-19 00:25:34 +0530

    My very first contest

---
+++ CodeChef/C06.cpp
@@ -0,0 +1,12 @@
+//My very first contest
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<a-b<<" "<<a-b-c<<endl;
+	return 0;
+}
