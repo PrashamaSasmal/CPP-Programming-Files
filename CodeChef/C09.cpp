be36661cae246f8acfed7e697d25a070c9ea1955
PrashamaSasmal <80055409+PrashamaSasmal@users.noreply.github.com>
2022-05-19 00:35:23 +0530

    Create C09.cpp

---
+++ CodeChef/C09.cpp
@@ -0,0 +1,27 @@
+//Recent contest problems
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	   int x,count1 = 0,count2=0;
+	 
+	   cin>>x;
+	   while(x--){
+	   string a;
+	   cin>>a;
+	    if(a== "START38")
+	    count1++;
+	    else
+	    count2++;
+  
+	   }
+	   cout<<count1<<" "<<count2<<endl;
+	   
+	}
+	return 0;
+}
